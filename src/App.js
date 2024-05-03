ccaf78ea013885cb8df4567b037e33873131dec2
Vigneshkumar <vignesh@tuna.group>
2024-05-03 14:00:57 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import './components/loginSignup';
+import LoginSignup from './components/loginSignup';
+
+function App() {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
